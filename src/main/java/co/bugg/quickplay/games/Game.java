8e8888ce2f6560e13d64ce3d92b04425bb09415d
bugfroggy <bugfroggy@gmail.com>
2017-12-11 23:21:21 -0500

    Simplified Game & Mode classes to allow for more remote customization

--- src/main/java/co/bugg/quickplay/games/Game.java
+++ src/main/java/co/bugg/quickplay/games/Game.java
@@ -13,11 +13,6 @@ public class Game implements Serializable {
      */
     public String name;
     /**
-     * Name of the Hypixel lobby for this game
-     * {@code null} for no lobby available
-     */
-    public String lobbyName;
-    /**
      * URL to the image file for this gamemode
      */
     public URL imageURL;
@@ -29,12 +24,10 @@ public class Game implements Serializable {
     /**
      * Constructor
      * @param name Display name of game
-     * @param lobbyName Lobby name for game
      * @param imageURL URL to image for game
      */
-    public Game(String name, String lobbyName, URL imageURL) {
+    public Game(String name, URL imageURL) {
         this.name = name;
-        this.lobbyName = lobbyName;
         this.imageURL = imageURL;
     }
 }
--- src/main/java/co/bugg/quickplay/games/Mode.java
+++ src/main/java/co/bugg/quickplay/games/Mode.java
@@ -14,23 +14,14 @@ public class Mode implements Serializable {
      * Command to play this mode
      */
     public String command;
-    /**
-     * Whether this mode's command is a normal /play command
-     * or if it's a different command (different commands are
-     * still specified in {@link #command}, just with
-     * the actual command prepended)
-     */
-    public boolean customCommand;
 
     /**
      * Constructor
      * @param name Display name of this mode
      * @param command Command for this mode
-     * @param customCommand Whether this mode has a custom command
      */
-    public Mode(String name, String command, boolean customCommand) {
+    public Mode(String name, String command) {
         this.name = name;
         this.command = command;
-        this.customCommand = customCommand;
     }
 }
