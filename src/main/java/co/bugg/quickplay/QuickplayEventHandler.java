966341cf04997f7d36efaec559fdd9cdb1396dc6
bugfroggy <bugfroggy@gmail.com>
2017-12-30 18:12:25 -0500

    Added check for null gui screen before rendering hud elements

--- src/main/java/co/bugg/quickplay/QuickplayEventHandler.java
+++ src/main/java/co/bugg/quickplay/QuickplayEventHandler.java
@@ -1,9 +1,7 @@
 package co.bugg.quickplay;
 
-import co.bugg.quickplay.client.gui.InstanceDisplay;
 import co.bugg.quickplay.util.ServerChecker;
-import net.minecraft.util.IChatComponent;
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
@@ -33,7 +31,11 @@ public class QuickplayEventHandler {
     @SubscribeEvent
     public void onRenderOverlay(RenderGameOverlayEvent event) {
         if(Quickplay.INSTANCE.onHypixel && event.type == RenderGameOverlayEvent.ElementType.TEXT) {
-            Quickplay.INSTANCE.instanceDisplay.render();
+            // Only render overlay if there is no other GUI open at the moment
+            // TODO Maybe make this into a config option?
+            if(Minecraft.getMinecraft().currentScreen == null) {
+                Quickplay.INSTANCE.instanceDisplay.render();
+            }
         }
     }
 }
