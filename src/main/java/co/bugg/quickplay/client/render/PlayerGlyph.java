5b0efb069cb0774f867f5344ba0ab9c13fa9e4f3
bugfroggy <bugfroggy@gmail.com>
2018-03-07 20:23:44 -0500

    fixed bug with mcResourceManager reload throwing NoSuchFieldException

--- src/main/java/co/bugg/quickplay/client/render/PlayerGlyph.java
+++ src/main/java/co/bugg/quickplay/client/render/PlayerGlyph.java
@@ -88,7 +88,12 @@ public class PlayerGlyph {
             httpResponse.close();
 
             // Reload the resource pack
-            final Field resourceManagerField = Minecraft.class.getDeclaredField("mcResourceManager");
+            Field resourceManagerField;
+            try {
+                resourceManagerField = Minecraft.class.getDeclaredField("field_110451_am");
+            } catch(NoSuchFieldException e) {
+                resourceManagerField = Minecraft.class.getDeclaredField("mcResourceManager");
+            }
             resourceManagerField.setAccessible(true);
             SimpleReloadableResourceManager resourceManager = (SimpleReloadableResourceManager) resourceManagerField.get(Minecraft.getMinecraft());
             resourceManager.reloadResourcePack(Quickplay.INSTANCE.resourcePack);
