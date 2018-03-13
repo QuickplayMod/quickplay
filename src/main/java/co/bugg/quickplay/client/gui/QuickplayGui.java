25552893c96b4bdee83f839c971717b8d64c7f7d
Erik Roberts <bugfroggy@gmail.com>
2018-03-13 18:52:14 -0400

    Fixed Quickplay not making any sounds

--- src/main/java/co/bugg/quickplay/client/gui/QuickplayGui.java
+++ src/main/java/co/bugg/quickplay/client/gui/QuickplayGui.java
@@ -5,11 +5,12 @@ import co.bugg.quickplay.QuickplayEventHandler;
 import co.bugg.quickplay.Reference;
 import com.google.common.collect.Lists;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvent;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
@@ -420,7 +421,7 @@ public class QuickplayGui extends GuiScreen {
             // Make a bunch of dumb noises
             Quickplay.INSTANCE.threadPool.submit(() -> {
                 while(Minecraft.getMinecraft().currentScreen == this) {
-                    mc.player.playSound(new SoundEvent(new ResourceLocation("mob.chicken.hurt")), 1.0F, 1.0F);
+                    mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.ENTITY_CHICKEN_HURT, 1.0F));
                     try {
                         Thread.sleep(200);
                     } catch (InterruptedException e) {
@@ -561,6 +562,6 @@ public class QuickplayGui extends GuiScreen {
      */
     public void componentClicked(QuickplayGuiComponent component) {
         // Play clicky sound
-        mc.player.playSound(new SoundEvent(new ResourceLocation("gui.button.press")), 1.0F, 1.0F);
+        mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.UI_BUTTON_CLICK, 1.0F));
     }
 }
