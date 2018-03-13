91a6c1da7c9c3ccf674d2e1e57cb322dcd58b9a0
Erik Roberts <bugfroggy@gmail.com>
2018-03-13 18:55:36 -0400

    Fixed sounds not working

--- src/main/java/co/bugg/quickplay/client/gui/QuickplayGui.java
+++ src/main/java/co/bugg/quickplay/client/gui/QuickplayGui.java
@@ -5,6 +5,7 @@ import co.bugg.quickplay.QuickplayEventHandler;
 import co.bugg.quickplay.Reference;
 import com.google.common.collect.Lists;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.EntityRenderer;
