76dbdf3ed828e41edd2396875b614aa19b1dfe2b
Erik Roberts <bugfroggy@gmail.com>
2018-03-26 13:26:24 -0400

    updated versions

--- src/main/java/co/bugg/quickplay/Reference.java
+++ src/main/java/co/bugg/quickplay/Reference.java
@@ -15,7 +15,7 @@ public class Reference {
     /**
      * Version of this forge mod
      */
-    public static final String VERSION = "2.0.1-beta.2";
+    public static final String VERSION = "2.0.1";
     /**
      * Google Analytics tracking ID
      */
