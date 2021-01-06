68a4533a9c57757ee17e8a525abfa246eccddb78
Erik <robere2@rpi.edu>
2021-01-05 23:15:35 -0500

    Updated version number

--- src/main/java/co/bugg/quickplay/Reference.java
+++ src/main/java/co/bugg/quickplay/Reference.java
@@ -17,7 +17,7 @@ public class Reference {
     /**
      * Version of this forge mod
      */
-    public static final String VERSION = "2.1.0-beta5-alpha9";
+    public static final String VERSION = "2.1.0-beta5-alpha10";
     /**
      * Google Analytics tracking ID
      */
